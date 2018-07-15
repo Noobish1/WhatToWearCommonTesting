9c1b8e8bf64e2f4b892b00c14097b2918a21dc30
Blair McArthur <blair.mcarthur@skipthedishes.com>
2018-07-15 19:16:35 +1200

    Initial Commit

---
+++ WhatToWearCommonTesting/WhatToWearCommonTesting.h
@@ -0,0 +1,19 @@
+//
+//  WhatToWearCommonTesting.h
+//  WhatToWearCommonTesting
+//
+//  Created by Blair McArthur on 15/07/18.
+//  Copyright © 2018 Noobish1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WhatToWearCommonTesting.
+FOUNDATION_EXPORT double WhatToWearCommonTestingVersionNumber;
+
+//! Project version string for WhatToWearCommonTesting.
+FOUNDATION_EXPORT const unsigned char WhatToWearCommonTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhatToWearCommonTesting/PublicHeader.h>
+
+
